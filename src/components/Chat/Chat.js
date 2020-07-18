0a26f41fa47be53b344d947ed0513078edc85168
unknown <kamalaujla4vr@gmail.com>
2020-07-18 15:47:48 +0530

    Completed chatting box

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,31 +1,67 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import queryString from 'query-string';
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
+import TextContainer from '../TextContainer/TextContainer';
+import Messages from '../Messages/Messages';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
 
 import './Chat.css';
 
-const ENDPOINT = 'localhost:5000';
 let socket;
 
-const Chat =({location})=>{
-    const [name,setName] = useState('');
-    const [room,setRoom] = useState('');
-    useEffect(()=>{
-        const {name, room} = queryString.parse(location.search);
+const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
 
-        socket = io(ENDPOINT);
-        setName(name);
-        setRoom(room);
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
 
-        socket.emit('join',{name,room})
+    socket = io(ENDPOINT);
 
-    },[ENDPOINT,location.search]);
+    setRoom(room);
+    setName(name)
+
+    socket.emit('join', { name, room }, (error) => {
+      if(error) {
+        alert(error);
+      }
+    });
+  }, [ENDPOINT, location.search]);
+  
+  useEffect(() => {
+    socket.on('message', message => {
+      setMessages(messages => [ ...messages, message ]);
+    });
     
-    
-    return(
-        <h1>Chat here</h1>
-    )
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+}, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if(message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  }
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+          <InfoBar room={room} />
+          <Messages messages={messages} name={name} />
+          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+      </div>
+      <TextContainer users={users}/>
+    </div>
+  );
 }
 
 export default Chat;
